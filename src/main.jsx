5c54d15e45fb0d0df6b1433c62be128c3f5ae4e5
Catizenship <evlessons@gmail.com>
2025-04-17 21:43:15 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import CatPassportGenerator from './CatPassportGenerator'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CatPassportGenerator from './CatPassportGenerator';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CatPassportGenerator />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
 
