8e686cba00e95ced5bbb30066a541d7046fe9153
Catizenship <evlessons@gmail.com>
2025-04-17 21:52:05 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CatPassportGenerator from './CatPassportGenerator';
+import CatPassportGenerator from './CatPassportGenerator.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
